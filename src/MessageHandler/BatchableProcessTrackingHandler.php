a6713ebfc8e16a31359ad6edb8de145a9cd8e776
Wojciech Gula <wojciechpiotr.gula@metro-markets.de>
2022-05-03 20:11:21 +0200

    Introducing Fibers example.

--- src/MessageHandler/BatchableProcessTrackingHandler.php
+++ src/MessageHandler/BatchableProcessTrackingHandler.php
@@ -5,6 +5,7 @@ namespace App\MessageHandler;
 use App\Entity\Tracking;
 use App\Message\BatchableProcessTracking;
 use App\Support\EntityManagerInterfaceAware;
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Messenger\Handler\Acknowledger;
 use Symfony\Component\Messenger\Handler\BatchHandlerInterface;
@@ -29,32 +30,48 @@ final class BatchableProcessTrackingHandler implements BatchHandlerInterface
         return $this->handle($message, $ack);
     }
 
-    /**
-     * @list<array{0: object, 1: Acknowledger}> $jobs
-     */
     private function process(array $jobs): void
     {
+        $asyncJobs = $this->initAsyncJobs($jobs);
+
+        do {
+            $asyncJobs = $asyncJobs->filter(fn(\Fiber $asyncJob) => $asyncJob->isRunning());
+        } while (!$asyncJobs->isEmpty());
+
+        $this->em->flush();
+    }
+
+    private function processSingleMessage(BatchableProcessTracking $message, Acknowledger $ack): void
+    {
+        try {
+            $this->httpClient->request(Request::METHOD_GET, 'http://localhost:8080/trackings/' . $message->getTrackingNumber());
+
+            $tracking = new Tracking();
+            $tracking->setTrackingNumber($message->getTrackingNumber());
+
+            $this->em->persist($tracking);
+
+            $ack->ack();
+
+        } catch (\Throwable $e) {
+            $ack->nack($e);
+        }
+    }
+
+    private function initAsyncJobs(array $jobs): ArrayCollection
+    {
+        $asyncJobs = new ArrayCollection();
         /**
          * @var BatchableProcessTracking $message
          * @var Acknowledger $ack
          */
         foreach ($jobs as [$message, $ack]) {
+            $asyncJob = new \Fiber(fn() => $this->processSingleMessage($message, $ack));
 
-            try {
-                $this->httpClient->request(Request::METHOD_GET, 'http://localhost:8080/trackings/' . $message->getTrackingNumber());
-
-                $tracking = new Tracking();
-                $tracking->setTrackingNumber($message->getTrackingNumber());
-
-                $this->em->persist($tracking);
-
-                $ack->ack();
-
-            } catch (\Throwable $e) {
-                $ack->nack($e);
-            }
-
-            $this->em->flush();
+            $asyncJob->start();
+            $asyncJobs->add($asyncJob);
         }
+
+        return $asyncJobs;
     }
 }
