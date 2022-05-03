6063dd875d5a3b50e46a07cbc384213811348d6e
Wojtek Gula <gulasz101@gmail.com>
2022-05-03 07:16:59 +0200

    Introducing extremely simplified dispatching and consuming messages. Getting rid of not needed any more code.

---
+++ src/Entity/Tracking.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TrackingRepository;
+use Carbon\CarbonImmutable;
+use Carbon\Doctrine\CarbonImmutableType;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\HasLifecycleCallbacks]
+#[ORM\Entity(repositoryClass: TrackingRepository::class)]
+class Tracking
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private int $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private string $tracking_number;
+
+    #[ORM\Column(type: 'carbon_immutable')]
+    private CarbonImmutable $created_at;
+
+    #[ORM\Column(type: 'carbon_immutable')]
+    private CarbonImmutable $modified_at;
+
+    #[ORM\PrePersist]
+    public function setCreatedAtValue(): self
+    {
+        if (!isset($this->created_at)) {
+            $this->created_at = CarbonImmutable::now();
+        }
+
+        return $this;
+    }
+
+    #[ORM\PostUpdate]
+    #[ORM\PrePersist]
+    public function updateModifiedAtValue(): self
+    {
+        $this->modified_at = CarbonImmutable::now();
+
+        return $this;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTrackingNumber(): ?string
+    {
+        return $this->tracking_number;
+    }
+
+    public function setTrackingNumber(string $tracking_number): self
+    {
+        $this->tracking_number = $tracking_number;
+
+        return $this;
+    }
+}
---
+++ src/Repository/TrackingRepository.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Tracking;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Tracking|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Tracking|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Tracking[]    findAll()
+ * @method Tracking[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class TrackingRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Tracking::class);
+    }
+
+    /**
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function add(Tracking $entity, bool $flush = true): void
+    {
+        $this->_em->persist($entity);
+        if ($flush) {
+            $this->_em->flush();
+        }
+    }
+
+    /**
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function remove(Tracking $entity, bool $flush = true): void
+    {
+        $this->_em->remove($entity);
+        if ($flush) {
+            $this->_em->flush();
+        }
+    }
+
+    // /**
+    //  * @return Tracking[] Returns an array of Tracking objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('t')
+            ->andWhere('t.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('t.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Tracking
+    {
+        return $this->createQueryBuilder('t')
+            ->andWhere('t.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
